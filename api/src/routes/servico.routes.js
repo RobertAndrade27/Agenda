1336981017a5a8ab9e8d067994dda672aeb8041d
Robert Andrade <robert_andrade@icloud.com>
2021-08-05 11:05:26 -0300

    feat

--- api/src/routes/servico.routes.js
+++ api/src/routes/servico.routes.js
@@ -1,15 +1,57 @@
 const express = require("express");
 const router = express.Router();
-const Busboy = require('busboy')
-
+const Busboy = require("busboy");
+const aws = require("../services/aws");
 const Salao = require("../models/salao");
 const Servico = require("../models/servico");
 
 router.post("/", async (req, res) => {
-  try {
-  } catch (err) {
-    res.json({ error: true, message: err.message });
-  }
+  let busboy = new Busboy({ headers: req.header });
+  busboy.on("finish", async () => {
+    try {
+      const { salaoId } = req.body;
+      let errors = [];
+      let arquivos = [];
+
+      // {
+      //   "312u89e2j198ue189e0": { ... }
+      //   "312u89e2j198ue189e0": { ... }
+      //   "312u89e2j198ue189e0": { ... }
+      //   "312u89e2j198ue189e0": { ... }
+      // }
+
+      if (req.files && Object.keys(req.files) > 0) {
+        for (let key of Object.keys(req.files)) {
+          const file = req.files[key];
+
+          //31231321.jpg
+
+          const nameParts = file.name.slit("."); //resultado [32342424, jpg]
+          const fileName = `${new Date().getTime()}.${
+            nameParts[nameParts.length - 1]
+          }`;
+          const path = `servicos/${salaoId}/${fileName}`;
+
+          const response = await aws.uploadToS3(file, path);
+
+          if(response.error){
+            errors.push({error: true, message: respsonse.message})
+          } else {
+            arquivos.push(path);
+          }
+
+        }
+      }
+
+      if(erros.length > 0) {
+        res.json(errors[0]);
+        return false;
+      }
+    } catch (err) {
+      res.json({ error: true, message: err.message });
+    }
+  });
+  req.pipe(busboy);
 });
 
 module.exports = router;
