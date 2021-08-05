24bbedc1b90534477665db965b0e3afdeab967b7
Robert Andrade <robert_andrade@icloud.com>
2021-08-05 15:58:44 -0300

    feat: put create

--- api/src/routes/servico.routes.js
+++ api/src/routes/servico.routes.js
@@ -2,18 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Busboy = require("busboy");
 const aws = require("../services/aws");
-const Salao = require("../models/salao");
 const Servico = require("../models/servico");
 const Arquivo = require("../models/arquivo");
 
 router.post("/", async (req, res) => {
-
   let busboy = new Busboy({ headers: req.headers });
 
   busboy.on("finish", async () => {
-
     try {
-      
       const { salaoId, servico } = req.body;
       let errors = [];
       let arquivos = [];
@@ -39,7 +35,6 @@ router.post("/", async (req, res) => {
 
           const response = await aws.uploadToS3(file, path);
 
-       
           if (response.error) {
             errors.push({ error: true, message: response.message });
           } else {
@@ -57,7 +52,7 @@ router.post("/", async (req, res) => {
       const servicoCadastrado = await Servico(jsonServico).save();
 
       arquivos = arquivos.map((arquivo) => ({
-        referenciaId: servicoCadastrado._id,
+        referenciaId: req.params.id,
         model: "Servico",
         caminho: arquivo,
       }));
@@ -65,6 +60,61 @@ router.post("/", async (req, res) => {
       await Arquivo.insertMany(arquivos);
 
       res.json({ servico: servicoCadastrado, arquivos });
+    } catch (err) {
+      res.json({ error: true, message: err.message });
+    }
+  });
+  req.pipe(busboy);
+});
+
+router.put("/:id", async (req, res) => {
+  let busboy = new Busboy({ headers: req.headers });
+
+  busboy.on("finish", async () => {
+    try {
+      const { salaoId, servico } = req.body;
+      let errors = [];
+      let arquivos = [];
+
+      if (req.files && Object.keys(req.files).length > 0) {
+        for (let key of Object.keys(req.files)) {
+          const file = req.files[key];
+
+          //31231321.jpg
+
+          const nameParts = file.name.split("."); //resultado [32342424, jpg]
+          const fileName = `${new Date().getTime()}.${
+            nameParts[nameParts.length - 1]
+          }`;
+          const path = `servicos/${salaoId}/${fileName}`;
+
+          const response = await aws.uploadToS3(file, path);
+
+          if (response.error) {
+            errors.push({ error: true, message: response.message });
+          } else {
+            arquivos.push(path);
+          }
+        }
+      }
+
+      if (errors.length > 0) {
+        res.json(errors[0]);
+        return false;
+      }
+      //  CRIAR SERVICO
+      const jsonServico = JSON.parse(servico);
+      await Servico.findByIdAndUpdate(req.params.id, jsonServico);
+
+      arquivos = arquivos.map((arquivo) => ({
+        referenciaId: req.params.id,
+        model: "Servico",
+        caminho: arquivo,
+      }));
+
+      await Arquivo.insertMany(arquivos);
+
+      res.json({ error: false });
     } catch (err) {
       res.json({ error: true, message: err.message });
     }
